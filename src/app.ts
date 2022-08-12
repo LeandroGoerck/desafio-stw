c2704e2540e1ada6a3b6c15ed8e431e370c157fb
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-12 19:05:48 -0300

    feat: configura servidor basico com typescript

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-MEthods', 'GET,POST, DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+    this.app.use(express.json());
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT);
+     console.log(`Servidor de receitas rodando na porta ${PORT}`);
+  }
+}
+
+export { App };
