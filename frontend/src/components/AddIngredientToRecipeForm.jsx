212a6b38d965bce62703e7b00d0ee1ce03daffc8
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-22 22:51:24 -0300

    cria formulario especifico para adicionar ingredientes as receitas

---
+++ frontend/src/components/AddIngredientToRecipeForm.jsx
@@ -0,0 +1,45 @@
+import { FilePlus } from "phosphor-react";
+import React, { useEffect, useState } from "react";
+import api from "../helpers/request";
+import IngredientSelect from "./IngredientSelect";
+
+export default function AddIngredientToRecipeForm(props) {
+  const { handleChanges, handleAddIngredientButton, form } = props;
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    api.get('/ingredients')
+      .then(({ data }) => {
+        setIngredients(data.ingredientsData);
+      })
+  }, [])
+
+  return (
+    <form className="flex flex-row ">
+      <div className="h-14 w-full mt-5 mb-5 bg-az1 flex flex-row items-center">
+
+        <IngredientSelect ingredients={ingredients} handleChanges={handleChanges}/>
+
+        <input
+          type="input"
+          name="previsto"
+          placeholder="Previsto em kg"
+          onChange={handleChanges}
+          value={form.descricaoIngrediente}
+          className="h-8 pl-1 w-2/4 rounded-sm">
+        </input>
+        
+        <button
+          type="button"
+          className="h-10 w-10 bg-white ml-2 mr-2 rounded-full flex flex-row items-center justify-center disabled:bg-cz1"
+          onClick={handleAddIngredientButton}
+          disabled={false}
+          >
+          <FilePlus size={28} />
+        </button>
+
+      </div>
+
+    </form>
+  );
+}
