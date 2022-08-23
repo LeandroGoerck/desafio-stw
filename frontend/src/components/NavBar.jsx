950af5e386abc21a40842ed8c7476f8fc9c30943
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-22 22:49:59 -0300

    adiciona link para a pagina principal no logo da navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav className="bg-az4 h-16 flex flex-row items-center">
       <div className="bg-white h">
+        <Link to={"/recipes"} >
         <img className="h-16" src="/stw.svg" alt="stw logo"></img>
+        </Link>
       </div>
       <div className="triangle"></div>
     </nav>
