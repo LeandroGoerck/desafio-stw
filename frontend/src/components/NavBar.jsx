e7caa9d2b88cff30f467a687daa9505401f86b46
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-23 00:17:36 -0300

    adiciona links para pagina de receitas e ingredientes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,11 +5,17 @@ export default function NavBar() {
   return (
     <nav className="bg-az4 h-16 flex flex-row items-center">
       <div className="bg-white h">
-        <Link to={"/recipes"} >
+        <Link to="/recipes" >
         <img className="h-16" src="/stw.svg" alt="stw logo"></img>
         </Link>
       </div>
       <div className="triangle"></div>
+      <Link to="/recipes">
+        <span className="text-xl text-white ml-5 mr-5">RECEITAS</span>
+      </Link>
+      <Link to="/ingredients">
+        <span className="text-xl text-white ml-5 mr-5">INGREDIENTES</span>
+      </Link>
     </nav>
   );
 }
