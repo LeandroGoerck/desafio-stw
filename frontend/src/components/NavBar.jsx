94a4bea769ab55250cc4b05d2da19d35cce7857d
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-29 23:13:03 -0300

    altera texto dos botões da navbar para icones quando a tela é pequena

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Clipboard, ClipboardText, ListPlus } from "phosphor-react";
 
 export default function NavBar() {
   return (
-    <nav className="bg-az4 h-16 flex flex-row items-center">
-      <div className="bg-white h">
-        <Link to="/recipes" >
-        <img className="h-16" src="/stw.svg" alt="stw logo"></img>
+    <nav className="bg-az4 flex flex-row h-16 items-center text-white">
+      <div className="bg-white h-16">
+        <Link to="/recipes">
+          <img className="h-16" src="/stw.svg" alt="stw logo"></img>
         </Link>
       </div>
       <div className="triangle"></div>
-      <Link to="/recipes">
-        <span className="text-xl text-white ml-5 mr-5">RECEITAS</span>
-      </Link>
-      <Link to="/ingredients">
-        <span className="text-xl text-white ml-5 mr-5">INGREDIENTES</span>
-      </Link>
-      <Link to="/create">
-        <span className="text-xl text-white ml-5 mr-5">CRIAÇÃO DE RECEITA</span>
-      </Link>
+      <div className="h-full w-full flex flex-row items-center justify-around">
+        <Link to="/recipes">
+          <Clipboard className="block md:hidden" size={32} />
+          <div className="h-fit text-xl p-5 hidden md:block">RECEITAS</div>
+        </Link>
+        <Link to="/ingredients">
+          <ClipboardText className="block md:hidden" size={32} />
+          <div className="h-fit text-xl p-5 hidden md:block">INGREDIENTES</div>
+        </Link>
+        <Link to="/create">
+          <ListPlus className="block md:hidden" size={32} />
+          <div className="h-fit text-xl p-5 hidden md:block">
+            CRIAÇÃO DE RECEITA
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
